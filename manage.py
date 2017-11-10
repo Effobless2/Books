4675bacc88b43032fe624f8d670323ed8e8639ec
Maxime Deboffle <effobless@hotmail.fr>
2017-11-10 10:25:47 +0100

    Marche pas

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 #! /usr/bin/env python3
 from flask import Flask
 app = Flask(__name__)
+app.debug = True
+
+from flask_script import Manager
+manager = Manager(app)
+
 @app.route("/")
 def home():
     return "<h1>Hello World</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
